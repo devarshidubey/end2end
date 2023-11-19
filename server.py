22ccfeea54cd1458fe78ddfde8133f1de9dbb29a
Devarshi <devdubey7582@gmail.com>
2023-11-20 04:38:27 +0530

    x3dh without signature

--- server.py
+++ server.py
@@ -1,55 +1,3 @@
-# from cryptography.hazmat.backends import default_backend
-# from cryptography.hazmat.primitives import serialization
-# from cryptography.hazmat.primitives.asymmetric import x25519
-# import socket
-
-# def load_private_key_from_file(filename='server_private_key.pem'):
-#     with open(filename, 'rb') as file:
-#         private_key_bytes = file.read()
-#         private_key = serialization.load_pem_private_key(
-#             private_key_bytes,
-#             password=None,
-#             backend=default_backend()
-#         )
-#     return private_key
-
-# def perform_key_exchange(private_key, other_public_key):
-#     shared_key = private_key.exchange(other_public_key)
-#     return shared_key
-
-# def start_server(server_port=12345):
-#     server_private_key = load_private_key_from_file()
-
-#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     server_socket.bind(('0.0.0.0', server_port))
-#     server_socket.listen(1)
-
-#     print("Server listening on port {}".format(server_port))
-
-#     while True:
-#         connection, client_address = server_socket.accept()
-        
-#         # Receive the public key in chunks until the entire key is received
-#         received_public_key_bytes = b""
-#         while len(received_public_key_bytes) < 32:
-#             chunk = connection.recv(32 - len(received_public_key_bytes))
-#             if not chunk:
-#                 break
-#             received_public_key_bytes += chunk
-
-#         # Convert the received public key bytes to a public key object
-#         received_public_key = x25519.X25519PublicKey.from_public_bytes(received_public_key_bytes)
-#         print(received_public_key_bytes.hex())
-#         # Perform key exchange
-#         shared_key = perform_key_exchange(server_private_key, received_public_key)
-
-#         print("Shared Key:", shared_key.hex())
-
-#         connection.close()
-
-# if __name__ == "__main__":
-#     start_server()
-
 import socket
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import x25519
@@ -66,8 +14,9 @@ def receive_public_key():
     
     with connection:
         data = connection.recv(4096)
-        public_key = serialization.load_pem_public_key(data, default_backend())
-        public_key_bytes = public_key.public_bytes(
+        #very very imp
+        public_key = serialization.load_pem_public_key(data, default_backend()) #very imp
+        public_key_bytes = public_key.public_bytes( 
             encoding=serialization.Encoding.Raw,
             format=serialization.PublicFormat.Raw
         )
--- test.py
+++ test.py
@@ -1,8 +1,34 @@
-import dh
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.backends import default_backend
 
-private_key1, _ = dh.generate_keys()
-n, public_key2 = dh.generate_keys()
-n1, public_key3 = dh.generate_keys()
+def generate_keys():
+    private_key = x25519.X25519PrivateKey.generate()
+    public_key = private_key.public_key()
+    return private_key, public_key
 
-print(private_key1.exchange(public_key2).hex())
-print(private_key1.exchange(public_key3).hex())
+if __name__ == "__main__":
+    ika_private, ika_public = generate_keys()
+    ikb_private, ikb_public = generate_keys()
+
+    spka_private, spka_public = generate_keys()
+    spkb_private, spkb_public = generate_keys()
+
+    eka_private, eka_public = generate_keys()
+    ekb_private, ekb_public = generate_keys()
+
+    dh1 = ika_private.exchange(spkb_public)
+    dh2 = eka_private.exchange(ikb_public)
+    dh3 = eka_private.exchange(spkb_public)
+
+    result1 = dh1 + dh2 + dh3
+
+    dh1 = spkb_private.exchange(ika_public)
+    dh2 = ikb_private.exchange(eka_public)
+    dh3 = spkb_private.exchange(eka_public)
+
+    result2 = dh1 + dh2 + dh3
+
+    if(result1 == result2):
+        print(result1.hex())
+        print(result2.hex())
