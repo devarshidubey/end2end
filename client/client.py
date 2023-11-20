7d985cfcee18d4ae233060225a143b97e204fd15
Devarshi <devdubey7582@gmail.com>
2023-11-20 05:57:25 +0530

    client publishes bundle when app is installed

---
+++ client/client.py
@@ -0,0 +1,109 @@
+import socket
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives import serialization
+import base64
+import json, os
+
+# Function to generate X25519 public key
+def generate_x25519_key():
+    private_key = x25519.X25519PrivateKey.generate()
+    public_key = private_key.public_key()
+    print(public_key.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    ).hex())
+    return private_key, public_key
+
+# Function to serialize X25519 public key to Base64
+def serialize_x25519_key(public_key):
+    serialized_key = public_key.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    )
+    base64_key = base64.b64encode(serialized_key).decode('utf-8')
+    return base64_key
+
+def publish_key_bundle(uuid, public_key1, public_key2, public_key3):
+    # Serialize X25519 public keys to Base64
+    base64_key1 = serialize_x25519_key(public_key1)
+    base64_key2 = serialize_x25519_key(public_key2)
+    base64_key3 = serialize_x25519_key(public_key3)
+
+    # Define the data structure
+    data_structure = {
+        "keys": [base64_key1, base64_key2, base64_key3],
+        "uuid": uuid
+        # You can include other information in the data structure as needed
+    }
+
+    # Convert data structure to JSON
+    json_data = json.dumps(data_structure)
+
+    # Connect to the server
+    server_address = ('localhost', 12345)  # Change this to your server's address
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(server_address)
+
+        # Send the JSON data
+        s.sendall(json_data.encode('utf-8'))
+
+        # You can add additional logic for receiving responses from the server if needed
+
+    # Note: This is a basic example and does not include error handling or encryption.
+    # In a production scenario, you should consider using TLS/SSL for secure communication.
+def store_local_keys():
+    storage_folder = os.path.join(os.getcwd(), "public_keys")
+    storage_folder2 = os.path.join(os.getcwd(), "private_keys")
+    os.makedirs(storage_folder, exist_ok=True)
+    os.makedirs(storage_folder2, exist_ok=True)
+    filenames1 = [
+            '1identity_public',
+            '2signed_public',
+            '3one_time_public'
+                    ]
+    filenames2 = [
+            '1identity_private',
+            '2signed_private',
+            '3one_time_private'
+                    ]
+    for i in range(3):
+        filename = os.path.join(storage_folder, f'{filenames1[i]}.txt')
+        filename1 = os.path.join(storage_folder2, f'{filenames2[i]}.txt')
+        private_key, public_key = generate_x25519_key()
+
+        with open(filename, 'wb') as key_file:
+            key_file.write(public_key.public_bytes(
+                encoding=serialization.Encoding.Raw,
+                format=serialization.PublicFormat.Raw
+            ))
+        with open(filename1, 'wb') as key_file:
+            key_file.write(private_key.private_bytes(
+                encoding=serialization.Encoding.Raw,
+            format=serialization.PrivateFormat.Raw,
+            encryption_algorithm=serialization.NoEncryption()
+            ))
+def read_local_keys():
+    keys = []
+    for key_file in os.listdir("public_keys"):
+        with open(os.path.join("public_keys", key_file), 'rb') as key_file:
+            key_bytes = key_file.read()
+            public_key = x25519.X25519PublicKey.from_public_bytes(key_bytes)
+            keys.append(public_key)
+    return keys
+
+def install_app():
+    if not os.path.isfile("installation"):
+        store_local_keys()
+        public_keys = read_local_keys()
+    # If the file doesn't exist, create it
+        with open("installation", "w") as installation_file:
+            installation_file.write("Installation file created.\n")
+        publish_key_bundle("uuid25519", public_keys[0], public_keys[1], public_keys[2])
+
+if __name__ == "__main__":
+    #when the app is FIRST installed, the below code will run
+    install_app()
+
+#NOTE TO SELF: 1. THE INSTALLATION FILE WILL CONTAIN DATE OF INSTALLATION, INFO ABOUT LAST TIME THE SIGNED PREKEY WAS UPDATED
+#2. BEFORE PUBLISHING THE KEYS DURING INSTALLATION, THE CLIENT MUST ASK THE SERVER TO RESERVE A UUID FOR ITSELF. THE SERVER WILL MAP THE UUID WITH THE IP ADDRESS(OR MAC ADDRESS) OF THE CLIENT, THE CLIENT WILL THEN STORE THAT UUID IN LOCAL MACHINE
+#3 UPLOAD THE SIGNATURE OF THE SIGNED PREKEY, SIGNED WITH IDENTITY KEY
